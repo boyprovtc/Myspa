17ba07ae74d79cbd587a69d71760afb0b1cbd11c
MinhTrieu <nguyenminhtrieu010@gmail.com>
2023-06-05 11:00:49 +0700

    fix

--- public/src/App.js
+++ public/src/App.js
@@ -5,8 +5,7 @@ import Home from './Pages/Home'
 import './App.css'
 import KH from './Pages/KH/KH'
 import { DH, LH, DSLH } from './Pages/DH/DH'
-
-import Container from './Pages/QLSP/Container'
+import SpBan from './Pages/QLSP/SpBan'
 
 
 
@@ -21,7 +20,7 @@ function App() {
         <Route path="/DH" exact Component={DH} />
         <Route path="/DH/LH" exact Component={LH} />
         <Route path="/DH/DSLH" exact Component={DSLH} />
-        <Route path="/QLSP/Container" exact Component={Container} />
+        <Route path="/QLSP/SpBan" exact Component={SpBan} />
       </Routes>
     </Router>
   )
