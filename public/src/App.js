9557e6df44a879fb0c4015a35345e0540fb7a22f
Nguyen Xuan Thanh <thanhxuan12086@gmail.com>
2023-06-07 09:57:25 +0700

    fix trang KH

--- public/src/App.js
+++ public/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Layout from './Components/Layout'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import './App.css'
-import KH from './Pages/KH/KH'
-import { DH, LH, DSLH } from './Pages/DH/DH'
-import SpBan from './Pages/QLSP/SpBan'
-
-
-
+import React from 'react';
+import Layout from './Components/Layout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import KH from './Pages/KH/KH';
+import { DH, LH, DSLH } from './Pages/DH/DH';
+import SpBan from './Pages/QLSP/SpBan';
+import SignIn from './Pages/KH/SignIn';
 
 function App() {
   return (
@@ -17,12 +16,13 @@ function App() {
       <Routes>
         <Route path="/home" exact Component={Home} />
         <Route path="/KH" exact Component={KH} />
+        <Route path="/themKH" exact Component={SignIn} />
         <Route path="/DH" exact Component={DH} />
         <Route path="/DH/LH" exact Component={LH} />
         <Route path="/DH/DSLH" exact Component={DSLH} />
         <Route path="/QLSP/SpBan" exact Component={SpBan} />
       </Routes>
     </Router>
-  )
+  );
 }
-export default App
+export default App;
