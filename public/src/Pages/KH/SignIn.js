d0a894a04dc16eddd6d36712540739d339a871ef
Nguyen Xuan Thanh <thanhxuan12086@gmail.com>
2023-06-07 09:38:24 +0700

    customer page

---
+++ public/src/Pages/KH/SignIn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './SignIn.css';
+
+function SignIn() {
+  return (
+    <div className="container">
+      <h1 className="text-warning">Đăng kí Khách hàng</h1>
+
+      <span className="icon">
+        <i class="bi bi-person-circle"></i>
+      </span>
+    </div>
+  );
+}
+
+export default SignIn;
